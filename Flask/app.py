f162d6fc65719fa5ba78bc9815182f5fad61626c
Prajwal <prajwalnikam4@gmail.com>
2025-04-26 21:38:50 +0530

    pillow

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,6 @@
 # importing libraries
 from flask import Flask, render_template, url_for, request
 from PIL import Image
-
 from keras.models import load_model # type: ignore
 from tensorflow.keras.preprocessing import image # type: ignore
 import numpy as np
